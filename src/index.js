16cbbcc63372ba0555204d620b5ae07684c1fadf
BradJKim <bradjkim03@gmail.com>
2024-06-07 22:12:41 -0700

    implemented user input and displaying api responses

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-import { getGiphy, getWeatherByCity } from "./utils/utils";
+import { getGiphy, getWeatherByCity, getLocationByCity } from "./utils/utils";
+import "./style.css";
 
-const response = await getWeatherByCity("London");
+const inputElement = document.getElementById("cityInput");
+inputElement.value = "";
 
-console.log(response);
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", async () => {
+    const inputElement = document.getElementById("cityInput");
 
-const phrase = "Hello World"
-const random = Math.floor(Math.random() * 50)
+    const cityName = inputElement.value;
+
+    const response = await getWeatherByCity(cityName);
+    console.log(response);
+
+    if (response === undefined) {
+        const error = document.getElementById("error");
+        error.classList.add("shown");
+    } else {
+        const error = document.getElementById("error");
+        error.classList.remove("shown");
+
+        const phrase = response.condition.text;
+        
+        const information = document.getElementById("information");
+        information.innerHTML = `Current Weather in ${cityName}: ${phrase}`;
+
+        const random = Math.floor(Math.random() * 50);
+
+        const gifurl = await getGiphy(phrase, random);
+
+        const element = document.getElementById("gif");
+        element.src = gifurl;
+    }
+});
+
+const phrase = "Hello World";
+const random = Math.floor(Math.random() * 50);
 
 const gifurl = await getGiphy(phrase, random);
 
